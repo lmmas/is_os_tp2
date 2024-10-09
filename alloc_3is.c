56826ddf81c5019cf02a048b7a0e1f08776bbd0c
lmmas <louismmassin@gmail.com>
2024-10-09 11:37:40 +0200

    question 2 finished

--- alloc_3is.c
+++ alloc_3is.c
@@ -9,16 +9,18 @@ typedef struct HEADER_TAG {
     long magic_number;
 } HEADER;
 
-void * malloc_3is(size_t dataSize) {
+long newMagicNumber = 0x0123456789ABCDEFL;
+HEADER * freeBlockListHead = NULL;
+HEADER * freeBlockList;
 
-    long newMagicNumber = 0x0123456789ABCDEFL;
+void * malloc_3is(size_t dataSize) {
 
     void * oldAddress = sbrk(sizeof(HEADER)+ dataSize + sizeof(long));
 
     HEADER* headerAddress = oldAddress;
-    HEADER newHeader = *headerAddress;
-    newHeader.block_size = dataSize;
-    newHeader.magic_number = newMagicNumber;
+    headerAddress->block_size = dataSize;
+    headerAddress->magic_number = newMagicNumber;
+    headerAddress->ptr_next = NULL;
 
     void * blockAddress = oldAddress + sizeof(HEADER);
 
@@ -29,20 +31,29 @@ void * malloc_3is(size_t dataSize) {
 }
 
 void allocTest() {
-    int arraySize = 2;
+    int arraySize = 8;
     int* testArray = malloc_3is(arraySize * sizeof(int));
     printf("address testArray : %p \n", testArray);
-    int* testArray2 = malloc(arraySize * sizeof(int));
-    printf("address testArray2 : %p \n", testArray2);
     for(int i = 0; i < arraySize; i++) {
         *(testArray + i) = i;
     }
-    long magicNumber = *(long*)(((void*) testArray) + arraySize*sizeof(int));
-    printf("magic number: %lX\n", magicNumber);
+    printf("test number: %d", *(testArray+ 4));
+    free_3is(testArray);
+    int arraySize2 = 9;
+    int * testArray2 = malloc_3is(arraySize2 * sizeof(int));
+    free_3is(testArray2);
 }
 
 void free_3is(void * address) {
-
-
-
+    HEADER* headerAddress = address;
+    headerAddress--;
+    if(freeBlockListHead == NULL) {
+        freeBlockListHead = headerAddress;
+        freeBlockList = freeBlockListHead;
+    }
+    else {
+        freeBlockList->ptr_next = headerAddress;
+        freeBlockList = freeBlockList->ptr_next;
+    }
 }
+
